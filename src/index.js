4249dcab2c119d7e9811e0d511d53bc3a186cc6a
alyshim <alyona.shimchuk@gmail.com>
2024-07-01 14:47:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateMocktail(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "33ftabad4a7f60213o971b6848003fd4";
   let context =
-    "You are a bartender experienced in mocktails. Your mission is to create an easy mocktail recipe and separate each line with a <br />. Make sure to follow the user instructions. Sign the mocktail recipe with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a bartender experienced in mocktails. Your mission is to create an easy mocktail recipe and separate each line with a <br />. Make sure to follow the user instructions.";
   let prompt = `User instructions: Generate a Mocktail recipe with ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt={prompt}&context={context}&key={apiKey}`;
 
